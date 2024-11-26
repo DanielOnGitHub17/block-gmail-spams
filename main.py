a5ab6f04b95f45b640da8bce08ce8f58bb7fe322
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-11-26 15:57:44 -0600

    function to click images in turn, to block spam senders. Added the images and assets too.

---
+++ main.py
@@ -0,0 +1,47 @@
+"""Script to loop all button clicks"""
+import pyautogui
+
+
+def main():
+    """If function ran as script. Will start/error when the user hits enter"""
+    print("Please open the spam gmails window/tab for the app to work")
+    n_spams = int(input("How many spam emails do you want deleted? "))
+    delete_spams(n_spams)
+
+
+def delete_spams(n):
+    """Code starts running from here"""
+    for _ in range(n):
+        click_menu()
+        click_block()
+        click_block_confirm()
+        click_next()  # Might not work the last time.
+
+
+def click_next():
+    """Function to click next button"""
+    click_button("next")
+
+
+def click_menu():
+    """Function to click menu button"""
+    click_button("menu")
+
+
+def click_block():
+    """Function to click block button"""
+    click_button("block")
+
+
+def click_block_confirm():
+    """Function to click block confirm button"""
+    click_button("block_confirm")
+
+
+def click_button(button_name, ext="png", duration=0.5):
+    pyautogui.moveTo(f"assets/{button_name}_button.{ext}", duration=duration)
+    pyautogui.click(interval=0.1)
+
+
+if __name__ == "__main__":
+    main()
